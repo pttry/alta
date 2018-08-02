c9904bff80aaafee35799c774fe6bd167d5f6af6
jlehtomaa <32097008+jlehtomaa@users.noreply.github.com>
2018-08-02 14:50:01 +0300

    Add files via upload

---
+++ oranig2013/test.bat
@@ -0,0 +1,16 @@
+
+REM Build model
+    tablo -pgs ORANIG
+REM Solve model using GEMSIM: homogenity test
+    gemsim -cmf htest.CMF
+    if errorlevel 1 goto err
+
+dir/od *.har
+dir/od *.sl4
+echo JOB DONE OK
+goto endbat
+:err
+echo ERROR
+:endbat
+
+pause
