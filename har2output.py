0162948c6f15831e7ae11cced0010bb015f7f393
Janne Huovari <janne.huovari@ptt.fi>
2019-01-06 17:20:13 +0200

    har2out

---
+++ har2output.py
@@ -0,0 +1,54 @@
+# -*- coding: utf-8 -*-
+"""
+Trasforming HAR file objects of output ready
+
+@author: jh
+"""
+
+# Basic modules
+import pandas as pd
+import numpy  as np
+import os
+
+import harpy
+
+class supplyTable:
+    """
+    A class to hold a supply table
+
+    Parameters
+    ----------
+    make : DataFrame
+        DataFrame for make matrix 
+    """
+    def __init__(self, make):
+        self.table = make           # should other parts
+        self.VT = np.matrix(make)
+
+
+class regSupplyTables:
+    """
+    A class to hold a regional supply table
+
+    Parameters
+    ----------
+    har_obj : HarFileObj
+        A HAR-file object from har file 
+    """
+    def __init__(self, har_obj):
+        self.ar = har_obj["array"]
+        self.dims = {k["name"]: k["dim_desc"] for k in har_obj["sets"]}
+        self.tables = {self.dims["DST"][i]: \
+                       supplyTable(pd.DataFrame(self.ar[:,:,i], \
+                                    columns = self.dims["IND"], index = self.dims["COM"]))\
+                        for i in range(len(self.dims["DST"]))}
+
+    def to_excel(self, file):
+        writer = pd.ExcelWriter(file, engine='xlsxwriter')
+        for key,values in self.tables.items():
+                values.table.to_excel(writer, sheet_name = key)
+        writer.save()
+
+
+
+
