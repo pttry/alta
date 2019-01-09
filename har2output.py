ed02d660034b3b37156dff76c049a15fff5dae60
Janne Huovari <janne.huovari@ptt.fi>
2019-01-09 10:38:22 +0200

    use table init

--- har2output.py
+++ har2output.py
@@ -62,18 +62,58 @@ class regSupplyTables:
 
 
 
-class useTableDom:
+class useTable:
     """
     A class to hold a use table
 
     Parameters
     ----------
-    use_dom : Datarame
+    use : Datarame
         DataFrame for a domestic use matrix including final use
     labour 1LAB
     capital 1CAP
     land 1LND
     taxes on production 1PTX
     """
-    def __init__(self, use_dom):
-        self
+    def __init__(self, use, va_labour, va_capital, va_land):
+        self.table = use
+        self.table = pd.concat([self.table, va_labour])
+        self.table = self.table[use.columns.tolist()]                     # Order to original
+
+
+class regUseTables:
+    """
+    A class to hold a regional supply table
+
+    Parameters
+    ----------
+    use_obj : HarFileObj
+        A USE object for a har-file
+    """
+    def __init__(self, use_obj, va_labour_obj, va_capital_obj, va_land_obj):
+        self.ar = use_obj["array"][:,0,:,:]
+        self.dims = {k["name"]: k["dim_desc"] for k in use_obj["sets"]}
+        self.va_labour = pd.DataFrame(va_labour_obj["array"].sum(axis = 1),
+                                         index = self.dims["USR"][0:len(self.dims["COM"])])
+        self.tables = {self.dims["DST"][i]: \
+                       useTable(use = pd.DataFrame(self.ar[:,:,i], \
+                                    columns = self.dims["USR"], index = self.dims["COM"]),\
+                                va_labour = self.va_labour[[i]].transpose(),\
+                                va_capital = va_capital_obj,\
+                                va_land = va_land_obj)\
+                        for i in range(len(self.dims["DST"]))}
+
+    def to_excel(self, file):
+        writer = pd.ExcelWriter(file, engine='xlsxwriter')
+        for key,values in self.tables.items():
+                values.table.to_excel(writer, sheet_name = key)
+        writer.save()
+
+
+premod = harpy.HarFileObj.loadFromDisk("TERM/premod.har")
+use_obj = premod.getHeaderArrayObj("BSMR")
+va_labour_obj = premod.getHeaderArrayObj("1LAB")
+va_capital_obj = premod.getHeaderArrayObj("1CAP")
+va_land_obj = premod.getHeaderArrayObj("1LND")
+reg_use = regUseTables(use_obj, va_labour_obj, va_capital_obj, va_land_obj)
+print(reg_use.tables["Uusimaa"].table)
