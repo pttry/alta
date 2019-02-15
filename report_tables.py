25b12ebda4400cb4f83ab1f2e0e12a96352b04e2
Naufal Alimov <naufal.alimov@ptt.fi>
2019-02-15 12:06:47 +0200

    report table BACK TO ORIGINAL

--- report_tables.py
+++ report_tables.py
@@ -61,193 +61,42 @@ make_obj["array"][:,:,:].sum()
 trade_obj["array"][:,0,:,:].sum()
 #equal to 
 use_obj["array"][:,0,:,:].sum() - tradmar_obj["array"][:,0,:,:,:].sum() + suppmar_obj["array"][:,:,:,:].sum()
+
 #Imports National:
 use_obj["array"][:,1,:,:].sum()-tradmar_obj["array"][:,1,:,:,:].sum()
 
+
 #Total national:
 #trade_obj["array"][:,1,:,:].sum()+use_obj["array"][:,1,33,:].sum()=use_obj["array"][:,1,:,:].sum()
-
-make_obj["array"][:,:,:].sum() + trade_obj["array"][:,1,:,:].sum()+use_obj["array"][:,1,33,:].sum()
 make_obj["array"][:,:,:].sum() + use_obj["array"][:,1,:,:].sum()-tradmar_obj["array"][:,1,:,:,:].sum()
 #equal to 
 use_obj["array"][:,:,:,:].sum() - tradmar_obj["array"][:,:,:,:,:].sum() + suppmar_obj["array"][:,:,:,:].sum()
 
-# domestic uusimaa: make+regional import=use-trad_mar+sup_mar+regional export
+
+#domestic uusimaa: VERSION1
 use_obj["array"][:,0,:,0].sum() + suppmar_obj["array"][:,:,:,0].sum() - tradmar_obj["array"][:,0,:,:,0].sum() + trade_obj["array"][:,0,0,1:].sum() 
 #equal to 
 make_obj["array"][:,:,0].sum() + trade_obj["array"][:,0,1:,0].sum() 
-trade_obj["array"][:,0,0,0].sum() + trade_obj["array"][:,0,0,1:].sum()
-
 
 # total uusimaa
 use_obj["array"][:,:,:,0].sum() - tradmar_obj["array"][:,:,:,:,0].sum() + suppmar_obj["array"][:,:,:,0].sum() + trade_obj["array"][:,0,0,1:].sum() 
 #equal to 
-make_obj["array"][:,:,0].sum() + trade_obj["array"][:,0,1:,0].sum() + trade_obj["array"][:,1,:,0].sum()+use_obj["array"][:,1,33,0].sum()
-
-#equal to 
 make_obj["array"][:,:,0].sum() + trade_obj["array"][:,0,1:,0].sum() + use_obj["array"][:,1,:,0].sum() - tradmar_obj["array"][:,1,:,:,0].sum()
 
-
-# total uusimaa
-use_obj["array"][:,:,:,:].sum() - tradmar_obj["array"][:,:,:,:,:].sum() + suppmar_obj["array"][:,:,:,:].sum() 
-
-#equal to 
-make_obj["array"][:,:,:].sum() + trade_obj["array"][:,1,:,:].sum()+use_obj["array"][:,1,33,:].sum()
-
-
-#################################
-#Margins all in make:
-
-# total uusimaa
-use_obj["array"][:,:,:,0].sum() + trade_obj["array"][:,0,0,1:].sum() 
-#equal to 
-make_obj["array"][:,:,0].sum() + trade_obj["array"][:,0,1:,0].sum() +use_obj["array"][:,1,:,0].sum() - tradmar_obj["array"][:,1,:,:,0].sum()- suppmar_obj["array"][:,:,:,0].sum() + tradmar_obj["array"][:,:,:,:,0].sum()
-
-# domestic uusimaa: make+regional import=use-trad_mar+sup_mar+regional export
-use_obj["array"][:,0,:,0].sum() + trade_obj["array"][:,0,0,1:].sum() 
-#equal to 
-make_obj["array"][:,:,0].sum() + trade_obj["array"][:,0,1:,0].sum() - suppmar_obj["array"][:,:,:,0].sum() + tradmar_obj["array"][:,0,:,:,0].sum()
-
-
-#Total national:
-make_obj["array"][:,:,:].sum() + trade_obj["array"][:,1,:,:].sum()+use_obj["array"][:,1,33,:].sum()
-make_obj["array"][:,:,:].sum() + use_obj["array"][:,1,:,:].sum()-tradmar_obj["array"][:,1,:,:,:].sum()
-#equal to 
-use_obj["array"][:,:,:,:].sum() 
-
-
-# Domestic national: 
-make_obj["array"][:,:,:].sum()+ tradmar_obj["array"][:,0,:,:,:].sum() - suppmar_obj["array"][:,:,:,:].sum()
-#equal to 
-use_obj["array"][:,0,:,:].sum() 
-
-
-###############################################################
-#Splitting make into make_own + make_regional (imports)
-#make_di---make internal or make_own
-
-
-#This is the identity in the example of Uusima
-make_obj["array"][:,:,0].sum()
-trade_obj["array"][:,0,0,:].sum()+suppmar_obj["array"][:,:,:,0].sum()
-
-
-for j in range(19):
-        for i in range(2):
-                trade_obj["array"][i+16,0,j,j] = trade_obj["array"][i+16,0,j,j]+suppmar_obj["array"][i,:,:,j].sum()
-                
-#now this is the identity
-make_obj["array"][:,:,0].sum()
-trade_obj["array"][:,0,0,:].sum()
-
-#make_own (make_di)
-make_di = np.zeros([30,30,19])
-for j in range(19):
-        for d in range(30):
-                for i in range(30):
-                        make_di[d,i,j]=make_obj["array"][d,i,j]/(make_obj["array"][d,:,j].sum())*trade_obj["array"][d,0,j,j]
-                        
-#make_drp is regional make or imports from other regions (rows summation will give that)                    
-make_drp = np.zeros([30,30,19])
-#make_drp2 = np.zeros([30,30,19])                 
-for j in range(19):
-        for d in range(30):
-                for i in range(30):
-                        #make_drp[d,i,j]=make_obj["array"][d,i,j]/(make_obj["array"][d,:,j].sum())*(make_obj["array"][d,:,j].sum() - trade_obj["array"][d,0,j,j])
-                        make_drp[d,i,j]=make_obj["array"][d,i,j]/(make_obj["array"][d,:,j].sum())*(trade_obj["array"][d,0,j,:].sum() - trade_obj["array"][d,0,j,j])
-                        #make_drp2[d,i,j]=make_obj["array"][d,i,j]/(make_obj["array"][d,:,j].sum())*(make_obj["array"][d,:,j].sum() - trade_obj["array"][d,0,j,j])
-
-#identities with make national                                
-make_di[:,:,:].sum()
-make_drp[:,:,:].sum()
-make_di[:,:,:].sum()+make_drp[:,:,:].sum()
-make_obj["array"][:,:,:].sum()
-
-#identities with make uusimaa 
-make_di[:,:,0].sum()
-make_drp[:,:,0].sum()
-make_di[:,:,0].sum()+make_drp[:,:,0].sum()
-make_obj["array"][:,:,0].sum()
-
-#restoring original trade matrix
-for j in range(19):
-        for i in range(2):
-                trade_obj["array"][i+16,0,j,j] = trade_obj["array"][i+16,0,j,j]-suppmar_obj["array"][i,:,:,j].sum()
-###############################################################
-
-#splitting domestic use into use_own (use_di) and use regional exports use_drp
-
-#the identity to be achieved in the example of uusimaa
-use_obj["array"][:,0,:,0].sum()
-trade_obj["array"][:,0,:,0].sum() +tradmar_obj["array"][:,0,:,:,0].sum()
-
-for j in range(19):
-        for i in range(30):
-                trade_obj["array"][i,0,j,j] = trade_obj["array"][i,0,j,j]+tradmar_obj["array"][i,0,:,:,j].sum()
-
-#now this is the identity
-use_obj["array"][:,0,:,0].sum()
-trade_obj["array"][:,0,:,0].sum()
-
-#use_di is use domestic own
-use_di = np.zeros([30,2,34,19])
-use_drp = np.zeros([30,2,34,19])
-for j in range(19):
-        for d in range(30):
-                for i in range(34):
-                        use_di[d,0,i,j]=use_obj["array"][d,0,i,j]/(use_obj["array"][d,0,:,j].sum())*trade_obj["array"][d,0,j,j]
-                        
-#use_drp is use domestic regional export (rows summation will give that)                    
-for j in range(19):
-        for d in range(30):
-                for i in range(34):
-                        use_drp[d,0,i,j]=use_obj["array"][d,0,i,j]/(use_obj["array"][d,0,:,j].sum())*(use_obj["array"][d,0,:,j].sum() - trade_obj["array"][d,0,j,j])
-                        
-
-#identities with use domestic national                            
-use_di[:,0,:,:].sum()
-use_drp[:,0,:,:].sum()
-use_drp2[:,0,:,:].sum()
-
-use_di[:,0,:,:].sum()+use_drp[:,0,:,:].sum()
-use_obj["array"][:,1,:,:].sum()
-
-
-#identities with use domestic uusimaa
-use_di[:,0,:,0].sum()
-use_drp[:,0,:,0].sum()
-use_di[:,0,:,0].sum()+use_drp[:,0,:,0].sum()
-use_obj["array"][:,0,:,0].sum()
-
-
-#restoring original trade matrix
-for j in range(19):
-        for i in range(30):
-                trade_obj["array"][i,0,j,j] = trade_obj["array"][i,0,j,j]-tradmar_obj["array"][i,0,:,:,j].sum()
-  
-###############################################################
-
-
-
-pd.DataFrame(trade_obj["array"][:,1,:,0].sum(axis = 1), columns = ["Imports_external"], index=trade_obj["sets"][0]["dim_desc"])
-
-pd.DataFrame(use_obj["array"][:,1,:,0].sum(axis = 1)- tradmar_obj["array"][:,1,:,:,0].sum(axis=(1,2)), columns = ["Imports_external"], index=use_obj["sets"][0]["dim_desc"])
-
-
-
-
+#######################################
 
 
 # regional tables
 importlib.reload(ho)
-reg_supp = ho.regSupplyTables(make_obj, trade_obj, use_obj, tradmar_obj)
+reg_supp = ho.regSupplyTables(make_di,make_drp trade_obj, use_obj, tradmar_obj)
 reg_use = ho.regUseTables(use_obj, trade_obj, tradmar_obj, suppmar_obj, va_labour_obj, va_capital_obj, va_land_obj)
 # Write to excel
 reg_supp.to_excel(file = "outdata/test_supp2014.xlsx")
 reg_use.to_excel(file = "outdata/test_use2014.xlsx")
 
 
-
+#######################################################################################################################################################################################
+#######################################################################################################################################################################################
 importlib.reload(ho)
 u_io = ho.build_io(reg_supp.tables["Uusimaa"], reg_use.tables["Uusimaa"])
 u_io.table
